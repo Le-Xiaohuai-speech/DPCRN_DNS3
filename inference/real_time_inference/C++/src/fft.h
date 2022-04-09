a3bcd36a21c0f5689d7a37c31b87993e1ea0728a
XiaohuaiLe-NCA <36794666+Le-Xiaohuai-speech@users.noreply.github.com>
2022-04-09 19:08:47 +0800

    Update fft.h

--- inference/real_time_inference/C++/src/fft.h
+++ inference/real_time_inference/C++/src/fft.h
@@ -12,3 +12,46 @@
 
 //Fast Fourier Transform
 #define M_PI 3.141592653589793238462643383279502
+
+
+static void make_sintbl(int n, float* sintbl)
+
+static void make_bitrev(int n, int* bitrev)
+
+
+/*
+* @breif Fast Fourier Transform
+* @x: real part
+* @y: imaginary part
+* @n: length of fft, negative for inverse FFT
+*/
+int fft(float* x, float* y, int n)
+
+/*
+* @brief: Short Time Fourier Transform
+* @s: input signal
+* @spec: spectrogram, N_frame * N_fft *2 (real and imag)
+* @win: window, window_length
+* @signal_length
+* @fft_length
+* @window_length
+* @hop_length
+*/
+void STFT(float *s, float *spec, float *win, int signal_length, int fft_length, int window_length, int hop_length)
+ 
+/*
+* @brief: inverse Short Time Fourier Transform
+* @s: output signal
+* @spec: spectrogram, N_frame * N_fft / 2 + 1 (rfft part) + N_fft/2 - 1 (rfft symmetrical part) 
+* @win: window, window_length
+* @N_frame: number of the frame
+* @signal_length: length of the input signal
+* @fft_length
+* @window_length
+* @hop_length
+*/
+void iSTFT(float *s, float *spec, float *win, int N_frame, int signal_length, int fft_length, int window_length, int hop_length)
+
+#endif
+
+
