e4db8189c3d0ef762697ab7c525b3deea0b0ddc6
XiaohuaiLe-NCA <36794666+Le-Xiaohuai-speech@users.noreply.github.com>
2022-04-09 18:26:13 +0800

    Update fft.h

--- inference/real_time_inference/C++/src/fft.h
+++ inference/real_time_inference/C++/src/fft.h
@@ -1,2 +1,14 @@
+/*
+ * @Author: Xiaohuaile
+ * @Date: 2022-4-9
+ * @Last Modified by: Xiaohuaile
+ */
 #ifndef FFT_H_
 #define FFT_H_
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cstdio>
+
+//Fast Fourier Transform
+#define M_PI 3.141592653589793238462643383279502
