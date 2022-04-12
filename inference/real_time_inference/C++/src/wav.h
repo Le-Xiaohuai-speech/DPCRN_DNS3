73ddff05ad04b174ff5757debbd2a6d4aaa966dd
XiaohuaiLe-NCA <36794666+Le-Xiaohuai-speech@users.noreply.github.com>
2022-04-12 17:09:08 +0800

    Update wav.h

--- inference/real_time_inference/C++/src/wav.h
+++ inference/real_time_inference/C++/src/wav.h
@@ -9,6 +9,18 @@
 #include <stdint.h>
 #include <stdio.h>
 
+// wav data
+struct wav_data{
+	public:
+		int16_t* data;
+		int size;
+
+		wav_data(){
+			data=NULL;
+			size=0;
+		}	
+};
+// wav header info 
 struct wav_info {
    uint_fast16_t num_channels;      /* 1 for mono, 2 for stereo, etc. */
    uint_fast16_t bits_per_sample;   /* 16 for CD, 24 for high-res, etc. */
@@ -16,7 +28,7 @@ struct wav_info {
    uint_fast32_t num_samples;       /* total number of samples per channel */
 };
 
-void read_wav_info(struct wav_info* w, FILE* fp);
+void read_wav_info(struct wav_info* w, wav_data* wdata, FILE* fp);
 /* Read wav_info from *fp, assuming *fp is a PCM format RIFF/WAVE file.
    Leaves the seek position of *fp at the beginning of the data section
    of *fp, so one could immediately begin reading/writing samples */
@@ -29,15 +41,12 @@ void write_wav_hdr(const struct wav_info* w, FILE* fp);
 void print_wav_info(const struct wav_info* w);
 /* Prints information from *w to stdout */
 
-void write_sample(const struct wav_info* w, FILE* fp, const int_fast32_t* sample);
-/* Write a sample to *fp in the correct Little Endian format.
-   sample should be an array with w->num_channels elements.
-   Note that we use the int_fast32_t datatype to hold samples, which should be
-   changed if you want to use bits_per_sample > 32.  Also, if you use, say,
-   bits_per_sample=24, then you want to make sure that your actual samples
-   are going to fit into a 3-byte Little Endian integer in twos-complement
-   encoding.  If you're only going to use bits_per_sample=16, you could
-   use int_fast16_t instead of int_fast32_t.  Note also that the WAVE
-   file format expects 8-bit samples to be UNsigned, so if you're going
-   to use bits_per_sample=8, then you should use uint_fast8_t to hold
-   your samples. */
+void write_file_wav(const struct wav_info* w, FILE* fp, const int16_t* sample);
+
+void free_source(wav_data* data);
+
+// read pcm data as .bin or .pcm
+void read_file_bin_data(const char *file, void *data, size_t byte_length);
+
+// write pcm data as .bin or .pcm
+void write_file_bin_data(const char *file, void *data, size_t byte_length);
